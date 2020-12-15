9f434716ee8a75e22c13fd0105673104af72c3f0
Alonso Trejos <atrejos@ing.uchile.cl>
2020-12-15 02:19:38 -0300

    unit tests

--- app/views.py
+++ app/views.py
@@ -1,60 +1,50 @@
 import re
-from flask import render_template
+from flask import render_template,request
 from app import app,mongo
 from bson.json_util import dumps
 
 @app.route('/')
 def home():
- #   query()
     return "hello"
 
 @app.route('/test')
 def test():
     return render_template('home.html')
 
-@app.route('/search_id',methods=['GET','POST'])
-def search_id():
-    query = request.args.get('query')
-    myquery = { "id": query }
-    results = mongo.products.find(myquery)
-    list_cur = list(results)
-    json_data = dumps(list_cur)
-    return json_data
-
-
 @app.route('/search_content',methods=['GET','POST'])
 def search_content():
+
     query = request.args.get('query')
-    myquery = { 
-            "$or":[
-                    { "brand":  { "$regex": query }},
-                    { "description": { "$regex": query }}
-                ] 
-            }
+    
+    #Si query es numerica puede ser busqueda de id
+    #Si no es numerica se aplica validacion de largo 3
+
+    if  (not query.isnumeric()) and len(query) < 3:
+        return "error largo_query"
+
+    if(query.isnumeric()):
+        myquery = { "id": int(query)  }
+    else:    
+        myquery = { 
+          "$or":[
+                 { "brand":  { "$regex": query }},
+                 { "description": { "$regex": query }}
+               ] 
+        }
     results = mongo.products.find(myquery)
     list_cur = list(results)
+
+    #Si es palindrome, aplicamos mitad de precio
+    if isPalindrome(query):
+        temp_list = list()
+
+        for el in list_cur:
+            el['price'] = halfPrice(el['price'])
+            temp_list.append(el)
+        list_cur = temp_list
+
     json_data = dumps(list_cur)
     return json_data
-   
-@app.route('/test_exact_brand',methods=['GET','POST'])
-def test_exact_brand():
-    query = "dsaasd"
-    myquery = { "brand": query }
-    results = mongo.products.find(myquery)
-    list_cur = list(results)
-    json_data = dumps(list_cur)
-    return json_data
-
-@app.route('/test_like_brand',methods=['GET','POST'])
-def test_like_brand():
-    query = "saa"
-    myquery = { "brand": { "$regex": query } }
-    results = mongo.products.find(myquery)
-    list_cur = list(results)
-    json_data = dumps(list_cur)
-    return json_data
-
-
 
 def isPalindrome(s):
     return s == s[::-1]
--- testing.py
+++ testing.py
@@ -1,6 +1,16 @@
 import unittest
+import pymongo
+import dns
+import requests
+import json
 from app import app
-from app.utils import isPalindrome,halfPrice
+from app import views
+
+client = pymongo.MongoClient("mongodb+srv://productListUser:productListPassword@cluster0.glk1i.mongodb.net/?retryWrites=true&w=majority",
+        tls=True,
+        tlsAllowInvalidCertificates=True)
+
+mongo = client['promotions']
 
 class BasicTests(unittest.TestCase):
     def setUp(self):
@@ -16,16 +26,52 @@ class BasicTests(unittest.TestCase):
         self.assertEqual(response.status_code,200)
 
     def test_isPalindromeTrue(self):
-        self.assertEqual(isPalindrome('asddsa'),True)
+        self.assertEqual(views.isPalindrome('asddsa'),True)
 
     def test_isPalindromeFalse(self):
-        self.assertEqual(isPalindrome('asdf'),False)      
+        self.assertEqual(views.isPalindrome('asdf'),False)      
 
     def test_halfPrice(self):
-        self.assertEqual(halfPrice(8),4)
+        self.assertEqual(views.halfPrice(8),4)
 
     def test_halfPrice(self):
-        self.assertEqual(halfPrice(5),3)
+        self.assertEqual(views.halfPrice(5),3)
+
+    def test_queryExistingId(self):
+        response = self.app.get('/search_content',query_string=dict(query='18') ,follow_redirects = True)
+        j = json.loads(response.data)
+        self.assertEqual(j[0]['id'],18) 
+
+    def test_tooSmallQuery(self):
+        response = self.app.get('/search_content',query_string=dict(query='ds') ,follow_redirects = True)
+        self.assertEqual(response.data,"error largo_query")
+
+    def test_exactBrand(self):
+        response = self.app.get('/search_content',query_string=dict(query='dsaasd') ,follow_redirects = True)
+        self.assertNotEqual(response.data,[]) 
+
+    def test_brandContains(self):
+        response = self.app.get('/search_content',query_string=dict(query='saa') ,follow_redirects = True)
+        self.assertNotEqual(response.data,[]) 
+    
+    def test_descriptionContains(self):
+        response = self.app.get('/search_content',query_string=dict(query='bvp') ,follow_redirects = True)
+        self.assertNotEqual(response.data,[]) 
+
+    def test_queryNonExistingId(self):
+        response = self.app.get('/search_content',query_string=dict(query='3001') ,follow_redirects = True)
+        j = json.loads(response.data)
+        self.assertEqual(j,[]) 
+
+    def test_queryNonExistingBrandOrDescription(self):
+        response = self.app.get('/search_content',query_string=dict(query='un elefante se balanceaba') ,follow_redirects = True)
+        j = json.loads(response.data)
+        self.assertEqual(j,[]) 
+
+    def test_queryHalfPrice(self):
+        response = self.app.get('/search_content',query_string=dict(query='dsaasd') ,follow_redirects = True)
+        j = json.loads(response.data)
+        self.assertEqual(j[0]['price'],65087) 
 
 
 if __name__ == "__main__":
